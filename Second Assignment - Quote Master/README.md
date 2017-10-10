0bc99034a9f4f1b262467245c2521bd92fa94585
Wee <chee.san.wee@intel.com>
2017-10-10 10:23:23 +0800

    Completed

---
+++ Second Assignment - Quote Master/README.md
@@ -0,0 +1,13 @@
+# Assignment 2 - Quote Master
+
+### [IMPORTANT] Install the npm packages required first:
+
+> npm install
+
+### To run the dev server:
+
+> npm run dev
+
+### To build the distributable files:
+
+> npm run build
---
+++ Second Assignment - Quote Master/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a quote to delete it!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        data: function(){
+            return {
+                maxQuotes: 10,
+                quotes: ['There is nothing noble in being superior to your fellow men. True nobility is being superior to your former self.']
+            };
+        },
+        methods: {
+            newQuote(quote){
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete some quotes first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index,1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+    }
+</script>
+
+<style>
+</style>
